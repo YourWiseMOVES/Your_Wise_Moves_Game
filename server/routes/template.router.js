947628c2d163ff1bd180c1aa5ce84e4c2b49289f
juliq <juliannq@mac.com>
2018-11-09 13:05:58 -0600

    get, post, delete and put requests to card table in database

--- server/routes/template.router.js
+++ server/routes/template.router.js
@@ -2,18 +2,68 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
-/**
- * GET route template
- */
+
+// GET info on all questions from the card table
 router.get('/', (req, res) => {
-    
+    console.log('get card');
+    pool.query(`SELECT * FROM "card";`)
+        .then((results) => {
+            res.send(results.rows)
+        }).catch((error) => {
+            console.log('Error GET /card', error);
+            res.sendStatus(500);
+        })
 });
 
-/**
- * POST route template
- */
+
+// POST for adding a new question to the card table in the database
 router.post('/', (req, res) => {
-
+    let body = req.body.card;
+    pool.query(`INSERT INTO "card"( "text","stage_id" )
+    VALUES ($1, $2);`,
+        [body.text,
+        body.stage_id])
+        .then(( results) => {
+            res.sendStatus(results.rows)
+        }).catch((error) => {
+            console.log('Error with server-side POST:', error);
+            res.sendStatus(500);
+        })
+       // (1, E'Put moves into practice in this moment. Map where you are, Open and breathe and listen to connect with emotions, feel your heart\'s desire and ask for clarity, Visualize what you really want, identify your next step to Engage it.', 5, TRUE),)
 });
+
+
+// DELETE question from card database
+router.delete('/', (req, res) => {
+    pool.query(`DELETE FROM "card"
+    WHERE "test"=$1 AND "stage_id"=$2;`,
+    [req.body.text,
+    req.body.stage_id])
+    .then((results) => {
+        res.sendStatus(200)
+    }).catch((error) => {
+        console.log('Error with server-side DELETE:', error);
+        res.sendStatus(500);
+    })
+});
+
+// PUT to edit a question
+router.put('/', (req, res) => {
+    const updatedCard = req.body;
+    console.log('in the edit function');
+    console.log(req.body);
+    pool.query(`UPDATE card
+    SET "text" = $1,
+    "stage_id" = $2,
+    WHERE "id"=$3`,
+    [req.body.text,
+    req.body.stage_id])
+    .then((results) => {
+        res.send(200)
+    }).catch((error) => {
+        console.log('Error with server-side PUT:', error);
+        res.send(500);
+    })
+})
 
 module.exports = router;
--- src/components/InfoPage/InfoPage.js
+++ src/components/InfoPage/InfoPage.js
@@ -1,16 +1,54 @@
 import React from 'react';
+import axios from 'axios';
+
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemTitle,
+  AccordionItemBody,
+} from 'react-accessible-accordion';
+
+import 'react-accessible-accordion/dist/fancy-example.css';
+
+
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-const InfoPage = () => (
-  <div>
-    <p>
-      Info Page
-    </p>
-  </div>
-);
+class InfoPage extends Component {
+
+  state = {
+    data: [],
+  }
+
+  
+
+  // <div>
+  //   <p>
+  //     Info Page
+  //   </p>
+  // </div>
+
+
+getCardInfo = () => {
+  axios.get('/api/card')
+  .then((response) => {
+    console.log('this is the respons for the card questions', response);
+    this.setState({ ...this.state,data: response.data }); // master list of all default cards
+    console.log(this.state);
+  }).catch((error) => {
+    console.log('error making get', error);
+  });
+}
+
+createQuestionPanels() {
+// map method to populate AccordionItems
+// will show the question and the move
+// give option to edit or delete
+}
+
+}
 
 export default InfoPage;
