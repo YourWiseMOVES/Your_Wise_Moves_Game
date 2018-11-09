4a18c74a3e6efea93305fe51afff9470de5ab881
Jake Marzitelli <mjakehs@gmail.com>
2018-11-09 14:25:20 -0600

    update start.js function to create two additional rows in sql db

--- server/game/modules/start.js
+++ server/game/modules/start.js
@@ -2,6 +2,7 @@
  * Run when a facilitator clicks a start game button
  * Generate a unique code for the game
  * Insert row into game table
+ * Create discussion phase and game state rows
 */
 const pool = require('../../modules/pool');
 const newCode = require('randomatic');
@@ -10,7 +11,13 @@ const start = async facilitatorId => {
     try {
         //create new game row, get the id back from db
         const gameId = await pool.query(`INSERT INTO "games" ('facilitator_id')
-        VALUES ($1) RETURNING "id";`, [facilitatorId]);
+            VALUES ($1) RETURNING "id";`, [facilitatorId]);
+        //create new discussion phase row using game id
+        const discussionId = pool.query(`INSERT INTO "discussion_phase" ("game_id") VALUES ($1)
+            RETURNING "id";`, [gameId]);
+        //create new game state row using game id and discussion id
+        pool.query(`INSERT INTO "game_state" ("game_id", "discussion_phase_id")
+            VALUES ($1, $2);`, [gameId, discussionId]);
         //use module to generate new six digit numerical code
         const code = await newCode('0', 6);
         //send the data back to the game function
---
+++ server/game/routes/game.router.js
@@ -0,0 +1,1 @@
+//routes to start, end game 
