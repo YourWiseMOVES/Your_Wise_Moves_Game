ca20166bb96ec616709ffe2ff9ce7d99275976c6
Tony Matthes <39568020+TonyMatthes@users.noreply.github.com>
2018-11-12 11:07:41 -0600

    Applies suggestion
    Co-Authored-By: juliq <juliannq@mac.com>

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
-app.use('/api/card', templateRouter)
+app.use('/api/card', cardRouter)
 
 // Serve static files
 app.use(express.static('build'));
