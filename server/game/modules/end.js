4074e37ae841ea51d4c0e6b0afd53ee3b0a29c01
Jake Marzitelli <mjakehs@gmail.com>
2018-11-14 01:11:30 -0600

    add temp storage for results

--- server/game/modules/end.js
+++ server/game/modules/end.js
@@ -6,16 +6,29 @@
  * close socket connections after all users have responded to the results prompt
 */
 
-const receiver = require('./receiver');
 const pool = require('../../modules/pool');
 
 const end = async (socket, gameId, link, io, code) => {
     try {
         //store this game's results in a separate temporary results table
-
+            //player id, player name, intention, all qs and as
+        let playerResponse = await pool.query('SELECT * FROM "player" WHERE "game_id"=$1;', [gameId]);
+        let players = playerResponse.rows;
+        for (let player of players) { //insert a results row for each player
+            let journalResponse = await pool.query('SELECT * FROM "journal" WHERE "id"=$1;', [player.journal_id])
+            journal = journalResponse.rows[0];
+            pool.query(`INSERT INTO "result"
+            ("player_id", "intention", "question_one", "response_one", "question_two", "response_two",
+            "question_three", "response_three", "question_four", "response_four", "question_five", "response_five"
+            )
+            VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12);
+            `, [player.id, player.intention, journal.question_one, journal.response_one, journal.question_two, journal.response_two,
+                journal.question_three, journal.response_three, journal.question_four, journal.response_four, journal.question_five, journal.response_five
+            ])
+        }
 
         //schedule an action to clear that table in however long
-    
+        
         
         //cascading delete on all temporary game data
         await pool.query(`DELETE FROM "game" WHERE "id"=$1;`, [gameId]);
