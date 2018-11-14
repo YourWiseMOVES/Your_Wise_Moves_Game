7d35bd0d9710a5e83518d4857d0eec55682c7641
juliq <juliannq@mac.com>
2018-11-13 18:29:50 -0600

    in the process of updating admin forms

--- src/components/AdminPage/AdminPage.js
+++ src/components/AdminPage/AdminPage.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import QuestionForm from './QuestionForm';
-import './InfoPage.css';
+// import QuestionForm from './QuestionForm';
+
 
 import {
   Accordion,
@@ -11,48 +11,48 @@ import {
 } from 'react-accessible-accordion';
 // 'Fancy' theme - boilerplate styles for all components:
 import 'react-accessible-accordion/dist/fancy-example.css';
-
+import FacilitatorForm from './FacilitatorForm';
 
 // This is one of our simplest components
 // It doesn't have local state, so it can be a function component.
 // It doesn't dispatch any redux actions or display any part of redux state
 // or even care what the redux state is, so it doesn't need 'connect()'
 
-class InfoPage extends Component {
+class AdminPage extends Component {
 
   state = {
     data: [],
   }
 
   componentDidMount() {
-    this.getCardInfo(); // call getCardInfo on page load to display all the default questions
+    this.getFacilitatorInfo(); // call getFacilitatorInfo on page load to display all the facilitators
   }
 
 
-  getCardInfo = () => {
-    axios.get('/api/card')
+  getFacilitatorInfo = () => {
+    axios.get('/api/register/')
       .then((response) => {
-        console.log('this is the response for the card questions', response);
-        this.setState({ ...this.state, data: response.data }); // master list of all default cards
+        console.log('this is the response for facilitators', response);
+        this.setState({ ...this.state, data: response.data }); // master list of all facilitators
         console.log(this.state);
       }).catch((error) => {
         console.log('error making get', error);
       });
   }
 
-  createHtmlForPanel(question) {
+  createHtmlForPanel(facilitator) {
     
       return (   //this creates the html for the panel for each question
 
         <AccordionItem>
           {/* style={{ border: '1px solid lightgray', color: 'gray', width: '70vw', align: 'center' }} */}
           <AccordionItemTitle >
-            <h5>{question.text} Stage: {question.stage_id}</h5>
+            <h4>{this.state.data.first_name} {this.state.data.last_name}</h4>
           </AccordionItemTitle>
           <AccordionItemBody>
-            <QuestionForm
+            <FacilitatorForm
               action="edit"  // updates the question form based on this function or add
-              question={question}  // javascript variable in html code
+              newFacilitator={this.state.data}  // javascript variable in html code
             />
           </AccordionItemBody>
         </AccordionItem>
@@ -62,26 +62,26 @@ class InfoPage extends Component {
   
 
   render() {
-    let questionCards = this.state.data.map(question => this.createHtmlForPanel(question)) // card created outside of the render
+    let facilitatorCards = this.state.data.map(facilitator => this.createHtmlForPanel(facilitator)) // card created outside of the render
 
     return (
 
       <div>
         <div className="preAccordion" >
-          <h4>1. To add a new question to database, enter it into the form.</h4>
-          <h4>2. To edit a question in the database: Select a question below.</h4>
+          <h4>1. To add a new facilitator to database, complete the form.</h4>
+          <h4>2. To edit a facilitator's status in the database, select a facilitator name below.</h4>
         </div>
         <div className="preAccordion">
-            <h4>Add a new question here:</h4>
+            <h4>Add a new facilitator here:</h4>
           </div>
         <div className="accordion">
-            <QuestionForm
+            <FacilitatorForm
               action="add"  // updates the question form based on this function or edit
             />
           </div>
         <div className="accordion">
           <Accordion>
-            {questionCards}
+            {facilitatorCards}
           </Accordion>
         </div>
         <div>
@@ -100,4 +100,4 @@ class InfoPage extends Component {
 }
 
 
-export default InfoPage;
+export default AdminPage;
--- src/components/RegisterPage/RegisterPage.js
+++ src/components/RegisterPage/RegisterPage.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+
+// import Input from '../InfoPage/Input';
+// import Select from "../InfoPage/Select";
+// import Button from "../InfoPage/Button";
+
+
 
 class RegisterPage extends Component {
   state = {
     username: '',
     password: '',
+    first_name: '',
+    last_name: '',
+    email: '',
+    organization: '',
+    phone_number: '',
+    is_facilitator: '', 
+    is_admin: '',
+
+    // const options = [
+    //   { value: 'true', label: 'True' },
+    //   { value: 'false', label: 'False' }
+    // ],
+
+    // is_facilitator: [
+    //   { value: false, label: 'False' },
+    //   { value: true, label: 'True' },
+    // ],
+    // is_admin: [
+    //   { value: false, label: 'False' },
+    //   { value: true, label: 'True' },
+    // ]
   };
 
   registerUser = (event) => {
@@ -16,10 +43,17 @@ class RegisterPage extends Component {
         payload: {
           username: this.state.username,
           password: this.state.password,
+          first_name: this.state.first_name,
+          last_name: this.state.last_name,
+          email: this.state.email,
+          organization: this.state.organization,
+          phone_number: this.state.phone_number,
+          is_facilitator: this.state.is_facilitator,
+          is_admin: this.state.is_admin,
         },
       });
     } else {
-      this.props.dispatch({type: 'REGISTRATION_INPUT_ERROR'});
+      this.props.dispatch({ type: 'REGISTRATION_INPUT_ERROR' });
     }
   } // end registerUser
 
@@ -28,6 +62,7 @@ class RegisterPage extends Component {
       [propertyName]: event.target.value,
     });
   }
+
 
   render() {
     return (
@@ -64,6 +99,100 @@ class RegisterPage extends Component {
               />
             </label>
           </div>
+
+          <div>
+            <label htmlFor="first_name">
+              First Name:
+            <input
+                type="text"
+                name="first_name"
+                value={this.state.first_name}
+                onChange={this.handleInputChangeFor('first_name')}
+              />
+            </label>
+          </div>
+
+          <div>
+            <label htmlFor="last_name">
+              Last Name:
+            <input
+                type="text"
+                name="last_name"
+                value={this.state.last_name}
+                onChange={this.handleInputChangeFor('last_name')}
+              />
+            </label>
+          </div>
+
+          <div>
+            <label htmlFor="email">
+              Email:
+            <input
+                type="text"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleInputChangeFor('email')}
+              />
+            </label>
+          </div>
+
+          <div>
+            <label htmlFor="organization">
+              Organization:
+            <input
+                type="text"
+                name="organization"
+                value={this.state.organization}
+                onChange={this.handleInputChangeFor('organization')}
+              />
+            </label>
+          </div>
+
+          <div>
+            <label htmlFor="phone_number">
+              Phone Number:
+            <input
+                type="number"
+                name="phone_number"
+                value={this.state.phone_number}
+                onChange={this.handleInputChangeFor('phone_number')}
+              />
+            </label>
+
+          </div>
+
+          <div>
+
+            <label htmlFor="is_facilitator">
+              Are you a Facilitator?
+            <select
+                type="text"
+                name="is_facilitator"
+                options={this.state.is_admin}
+                value={this.state.is_facilitator.boolean}
+                Placeholder={"True or False"}
+                onChange={this.handleInputChangeFor('is_facilitator')}
+              />
+            </label>
+
+          </div>
+
+          <div>
+
+            <label htmlFor="is_admin">
+              Are you an administrator?
+            <select
+                type="text"
+                name="is_admin"
+                options={this.state.is_admin}
+                value={this.state.is_admin.boolean}
+                Placeholder={"True or False"}
+                onChange={this.handleInputChangeFor('is_admin')}
+              />
+            </label>
+
+          </div>
+
           <div>
             <input
               className="register"
@@ -73,23 +202,23 @@ class RegisterPage extends Component {
             />
           </div>
         </form>
-        <center>
-          <button
-            type="button"
-            className="link-button"
-            onClick={() => {this.props.dispatch({type: 'SET_TO_LOGIN_MODE'})}}
-          >
-            Login
+        {/* <center> */}
+        <button
+          type="button"
+          // className="link-button"
+          onClick={() => { this.props.dispatch({ type: 'SET_TO_LOGIN_MODE' }) }}
+        >
+          Login
           </button>
-        </center>
-      </div>
+        {/* </center> */}
+      </div >
     );
   }
 }
 
 // Instead of taking everything from state, we just want the error messages.
 // if you wanted you could write this code like this:
-// const mapStateToProps = ({errors}) => ({ errors });
+// const mapStateToProps = ({errors}) => ({errors});
 const mapStateToProps = state => ({
   errors: state.errors,
 });
