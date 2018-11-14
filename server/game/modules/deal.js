4acb8cdeb66c8991462fb0699247f77b053f1d7b
Jake Marzitelli <mjakehs@gmail.com>
2018-11-14 00:02:12 -0600

    server side card dealing handler

--- server/game/modules/deal.js
+++ server/game/modules/deal.js
@@ -25,7 +25,27 @@ const pool = require('../../modules/pool');
 
 const deal =  async (action, gameId, socket) => {
     try {
-       
+        //get cards
+       let cardResponse  = await pool.query(`SELECT * FROM "card";`)
+       //get players
+       let playerResponse = await pool.query(`SELECT "id" FROM "player" WHERE "game_id"=$1;`, [gameId])
+       //set cards as card data
+       let cards = cardResponse.rows;
+       //set players as players data
+       let players = playerResponse.rows;
+       //create a deck object with Deck class
+       let deck =  new Deck(cards);
+       //set selected deck by round number
+       deck.subSelect(Number(action.data.roundNumber));
+       for (let player of players) {
+           let draw = deck.draw(); //draw a card
+           //set the players card in the database
+           pool.query(`UPDATE "player" SET "current_card"=$1 WHERE "id"=$2;`, [draw.text, player.id])
+           deck.shuffle(); //shuffle the deck (for fun)
+       }
+       //emit players event to trigger client player updates
+       socket.emit('players', {type: 'done'});
+       socket.broadcast.emit('players', {type: 'done'})
     }   
     catch (err) {
         console.log('Error in deal handler', err);
