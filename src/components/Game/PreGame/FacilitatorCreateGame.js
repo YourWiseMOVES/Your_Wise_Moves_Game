307a875f0ec9b5104cb9f9c66eb4b2b4f4cf3b13
Jake Marzitelli <mjakehs@gmail.com>
2018-11-16 13:30:08 -0600

    add game management for facilitators, active status for players in game

--- src/components/Game/PreGame/FacilitatorCreateGame.js
+++ src/components/Game/PreGame/FacilitatorCreateGame.js
@@ -10,26 +10,51 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class FacilitatorCreateGame extends Component {
+
+  state = {
+    name: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      ...this.state,
+      [event.target.name]: event.target.value,
+    })
+  }
+
+  componentDidMount(){
+    this.props.dispatch({type: 'FETCH_GAMES'});
+  }
+
   render() {
     return (
       <div>
-        <h1>Create New Game</h1>
+        <h1>Manage Games</h1>
         <h2>Facilitator View</h2>
-        <h3>Facilitator chooses settings for new game</h3>  
-        <h2>Your Code: {this.props.gameCode}</h2>
-        <button onClick={this.props.createGame}>Create Game</button>
+        <label>
+          Name your game!
+          <input onChange={this.handleChange} placeholder="Name" name="name" value={this.state.name}></input>
+        </label>
+        <button onClick={() => this.props.createGame(this.state.name)}>Create A New Game</button>
         <ol>
+          {this.props.state.games.map(game => {
+            return(
+              <li key={game.id}>{game.name}, {game.code}, {game.players} players, {game.active} active <button onClick={() => this.props.facilitatorJoinGame(game)}>Join</button></li>
+            );
+          })}
+        </ol>
+        {/* <ol>
           {this.props.state.game.allPlayers.map(player => {
             return(
               <li key={player.id}>{player.name}</li>
             )
           })}
-        </ol>   
+        </ol>    */}
         {/* 
         routing here currently works by checking redux state if there is a game code, to advance
         a facilitator sets the game code in their own redux state
         */}
-        <button onClick={() => this.props.dispatch({type: 'SET_CODE', payload: this.props.gameCode})}>Proceed to Game</button>
+        {/* <button onClick={() => this.props.dispatch({type: 'SET_CODE', payload: this.props.gameCode})}>Proceed to Game</button> */}
       </div>
     );
   }
