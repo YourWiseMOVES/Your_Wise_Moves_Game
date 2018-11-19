1361f8dbc982e1f4d82b5f7ea4e3910148f335c5
Jake Marzitelli <mjakehs@gmail.com>
2018-11-19 10:51:09 -0600

    create facilitator option selects in sidebar

--- server/game/modules/start.js
+++ server/game/modules/start.js
@@ -7,13 +7,16 @@
 const pool = require('../../modules/pool');
 const newCode = require('randomatic');
 
-const start = async (facilitatorId, name) => {
+//changes needed -> all settings changed in database, allow options to be accessible throughout game modules
+//check the database to make sure the code is unique before creating the new game with it
+
+const start = async (facilitatorId, config) => {
     try {
         //use module to generate new six digit numerical code
         let code = await newCode('0', 6);
         //create new game row, get the id back from db
         let gameId = await pool.query(`INSERT INTO "game" ("facilitator_id", "name", "code")
-            VALUES ($1,$2,$3) RETURNING "id";`, [facilitatorId, name, code]);
+            VALUES ($1,$2,$3) RETURNING "id";`, [facilitatorId, config.name, code]);
         gameId = gameId.rows[0].id
         //create new game state row using game id
         pool.query(`INSERT INTO "game_state" ("game_id")
