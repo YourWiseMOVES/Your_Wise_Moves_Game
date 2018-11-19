f56463a23338a1be5c50acee5a867ce71c866d5a
juliq <juliannq@mac.com>
2018-11-19 15:51:01 -0600

    journal question buttons

--- src/components/Game/Journal.js
+++ src/components/Game/Journal.js
@@ -9,6 +9,15 @@ class Journal extends Component {
         this.question_four.style.display = 'none';
         this.question_five.style.display = 'none';
 
+        this.question_one_tab.classList.remove('journal-active')
+        this.question_two_tab.classList.remove('journal-active')
+        this.question_three_tab.classList.remove('journal-active')
+        this.question_four_tab.classList.remove('journal-active')
+        this.question_five_tab.classList.remove('journal-active')
+
+        let tabKey = questionKey + '_tab';
+
+        this[tabKey].classList.add('journal-active');
         this[questionKey].style.display = 'block';
     }
 
@@ -19,42 +28,42 @@ class Journal extends Component {
     render() {
         return (
             <div>
-                <h2>Journal</h2>
+                <h3 className="journal">Journal</h3>
 
                 <div className="tab">
-                    <button className="tablinks" onClick={() => this.openQuestionNumber('question_one')}>Question One</button>
-                    <button className="tablinks" onClick={() => this.openQuestionNumber('question_two')}>Question Two</button>
-                    <button className="tablinks" onClick={() => this.openQuestionNumber('question_three')}>Question Three</button>
-                    <button className="tablinks" onClick={() => this.openQuestionNumber('question_four')}>Question Four</button>
-                    <button className="tablinks" onClick={() => this.openQuestionNumber('question_five')}>Question Five</button>
+                    <button ref={ref => this.question_one_tab = ref} className="tablinks" onClick={() => this.openQuestionNumber('question_one')}>Question One</button>
+                    <button ref={ref => this.question_two_tab = ref} className="tablinks" onClick={() => this.openQuestionNumber('question_two')}>Question Two</button>
+                    <button ref={ref => this.question_three_tab = ref} className="tablinks" onClick={() => this.openQuestionNumber('question_three')}>Question Three</button>
+                    <button ref={ref => this.question_four_tab = ref} className="tablinks" onClick={() => this.openQuestionNumber('question_four')}>Question Four</button>
+                    <button ref={ref => this.question_five_tab = ref} className="tablinks" onClick={() => this.openQuestionNumber('question_five')}>Question Five</button>
                 </div>
 
                 <div id="QuestionOne" ref={ref => this.question_one = ref} className="tabcontent">
-                    <h3>Question One</h3>
+                    <h4>Map</h4>
                     <p>{this.props.journal.question_one}</p>
                     <p>{this.props.journal.response_one}</p>
                 </div>
 
                 <div id="QuestionTwo" ref={ref => this.question_two = ref} className="tabcontent">
-                    <h3>Question Two</h3>
+                    <h4>Open</h4>
                     <p>{this.props.journal.question_two}</p>
                     <p>{this.props.journal.response_two}</p>
                 </div>
 
                 <div id="QuestionThree" ref={ref => this.question_three = ref} className="tabcontent">
-                    <h3>Question Three</h3>
+                    <h4>Visualize</h4>
                     <p>{this.props.journal.question_three}</p>
                     <p>{this.props.journal.response_three}</p>
                 </div>
 
                 <div id="QuestionFour" ref={ref => this.question_four = ref} className="tabcontent">
-                    <h3>Question Four</h3>
+                    <h4>Engage</h4>
                     <p>{this.props.journal.question_four}</p>
                     <p>{this.props.journal.response_four}</p>
                 </div>
 
                 <div id="QuestionFive" ref={ref => this.question_five = ref} className="tabcontent">
-                    <h3>Question Five</h3>
+                    <h5>Sustain</h5>
                     <p>{this.props.journal.question_five}</p>
                     <p>{this.props.journal.response_five}</p>
                 </div>
