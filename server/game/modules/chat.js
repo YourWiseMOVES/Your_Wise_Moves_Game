220f1925dda970234bc971713945edec91967684
Jake Marzitelli <mjakehs@gmail.com>
2018-11-19 14:44:07 -0600

    create chat handler on server

--- server/game/modules/chat.js
+++ server/game/modules/chat.js
@@ -1,15 +1,24 @@
 /**  Function Should:
+ * take in a chat message
+ * update the messages table
+ * tell all clients to update messages
 */
 
 const pool = require('../../modules/pool');
 
 //the structure for what a chat message will look like
 const sampleMessage = {
+    message: '',
+    type: '',
 }
 
-const chat =  async (action, gameId, socket, config) => {
+const chat =  async (message, gameId, socket, config) => {
     try {
-        
+        await pool.query(`INSERT INTO "chat" ("message", "type", "game_id") VALUES ($1, $2, $3);`,
+            [message.message, message.type, gameId]
+        ) //update the chat table
+        socket.broadcast.emit('chat', {done: true});
+        socket.emit('chat', {done: true});
     }   
     catch (err) {
         console.log('Error in chat handler', err);
