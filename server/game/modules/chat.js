0945fc46293b9b22702d9fb86ae8f6caf86d992b
Jake Marzitelli <mjakehs@gmail.com>
2018-11-19 14:37:03 -0600

    add chat handler structure

---
+++ server/game/modules/chat.js
@@ -0,0 +1,20 @@
+/**  Function Should:
+*/
+
+const pool = require('../../modules/pool');
+
+//the structure for what a chat message will look like
+const sampleMessage = {
+}
+
+const chat =  async (action, gameId, socket, config) => {
+    try {
+        
+    }   
+    catch (err) {
+        console.log('Error in chat handler', err);
+    }
+}
+
+
+module.exports = chat;
