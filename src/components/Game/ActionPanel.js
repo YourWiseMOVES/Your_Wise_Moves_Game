35d49a86ec8ceee67c98ac38e4c9bb5aeb49aeb1
Jake Marzitelli <mjakehs@gmail.com>
2018-11-21 09:41:52 -0600

    clean up action panel component

--- src/components/Game/ActionPanel.js
+++ src/components/Game/ActionPanel.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Journal from './Journal';
-import FacilitatorSidebar from './FacilitatorSidebar';
 
 
-class Sidebar extends Component {
+
+class ActionPanel extends Component {
     state = {
         trigger: false,
     }
 
-    triggerSidebar = (event) => {
+    triggerActionPanel = (event) => {
         if (this.state.trigger === false) {
-            console.log('Hello');
             this.actionPanel.className = "actionPanel showActionPanel"
             event.target.className = "actionPanelButton moveActionPanelButton"
             this.setState({
                 trigger: !this.state.trigger,
             })
         } else {
-            console.log('goodbye');
             event.target.className = "actionPanelButton moveActionPanelButtonBack"
             this.actionPanel.className = "actionPanel hideActionPanel"
             this.setState({
@@ -40,4 +37,4 @@ const mapStateToProps = state => ({
     state,
 });
 
-export default connect(mapStateToProps)(Sidebar);
+export default connect(mapStateToProps)(ActionPanel);
