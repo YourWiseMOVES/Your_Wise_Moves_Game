f172e8516e2229ac0e92e28ee7baa4c41582902d
Jake Marzitelli <mjakehs@gmail.com>
2018-11-25 09:07:53 -0600

    add home screen component for player view

---
+++ src/components/Game/PlayInstructions.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+
+class PlayInstructions extends Component {
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = state => ({
+    state,
+});
+
+export default connect(mapStateToProps)(PlayInstructions);
